a012f99f951e0ab17ab6cb2737527824be7c54e6
songjunhyen <songjunhyen@gmail.com>
2024-04-23 14:03:18 +0900

    세팅

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -2,11 +2,8 @@ package com.ll;
 
 public class Main {
   public static void main(String[] args) {
-    int a = 2;
-    int b = 91;
+    System.out.println("hi");
 
-    System.out.printf("%d%d\n",a,b);
-    System.out.println(2*a*b);
   }
 }
 
